b77d2a5797982b99c4d69af9a9db97a2afac6733
Anuanushree <anuabi2408@gmail.com>
2024-08-18 14:42:31 +0530

    chking the error 5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Define your Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: "terser", // Minification tool to use, options are 'terser' or 'esbuild'
+    terserOptions: {
+      compress: {
+        drop_console: true, // Optionally remove console statements
+      },
+    },
+    sourcemap: true, // Enable source maps for easier debugging
+  },
+});
